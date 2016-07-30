3a2cbd7b3822648e66c381c601e16d4686137c31
115100 <115100@users.noreply.github.com>
2016-07-30 14:33:20 +0100

    Safe download.

--- install_scripts.sh
+++ install_scripts.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+set -e
 
-curl -o /usr/local/sbin/openup https://stable.mtier.org/openup
+curl -o /tmp/openup https://stable.mtier.org/openup
+
+# Last known good version
+[ `sha512 -bq /tmp/openup` == 'hCkylHhmg6VXJGzzeClrXkjTcpC+SlmvlLURvFb0gaWJRDhaSyDu0DqqrKMV74tDidAiOClO6BkzQBpgncfnvg==' ]
+
+chmod +x /tmp/openup && mv /tmp/openup /usr/local/sbin
+
 echo '/usr/local/sbin/openup' >> /etc/daily
